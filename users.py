24c43be4c23222a63fb41b0092684d00cc62e117
alishersultansiyk06-creator <alishersultansiyk06@gmail.com>
2026-04-12 20:56:32 +0500

    Update users.py

--- users.py
+++ users.py
@@ -1,1 +1,15 @@
+# users.py
 
+# Пайдаланушылар туралы деректерді осында сақтаймыз
+USERS = {
+    'admin': {
+        'name': 'Биғалиева Венера',
+        'role': 'teacher',
+        'dashboard': 'teacher_dashboard'
+    },
+    'student': {
+        'name': 'Сұлтансиық Әлішер',
+        'role': 'student',
+        'dashboard': 'student_dashboard'
+    }
+}
