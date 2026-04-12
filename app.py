0e59347eaa1b01dc6f387ed65ecc051f88d38744
alishersultansiyk06-creator <alishersultansiyk06@gmail.com>
2026-04-12 18:18:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import os
+from datetime import timedelta
 
 app = Flask(__name__)
-app.secret_key = 'bilim_iq_2026' # Тұрақты кілт
+app.secret_key = 'alisher_secret_2026' # Тұрақты кілт
+app.permanent_session_lifetime = timedelta(days=7) # 7 күнге сақтау
 
-# Пайдаланушылар базасы (сенің скриншотың бойынша)
+# Пайдаланушылар
 users = {
     "student": {"password": "123", "role": "student"},
     "admin": {"password": "123", "role": "teacher"}
@@ -19,22 +20,25 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+        # HTML-дегі name="username" және name="password" арқылы аламыз
+        user_input = request.form.get('username')
+        pass_input = request.form.get('password')
         
-        if username in users and users[username]['password'] == password:
+        if user_input in users and users[user_input]['password'] == pass_input:
             session.permanent = True
-            session['username'] = username
+            session['username'] = user_input
             return redirect(url_for('student_dashboard'))
         else:
-            return "Қате логин немесе пароль!"
+            return "Қате! Қайтадан көріңіз."
+            
     return render_template('login.html')
 
 @app.route('/student_dashboard')
 def student_dashboard():
     if 'username' not in session:
         return redirect(url_for('login'))
-    # Бос тізімдерді жіберу, әйтпесе Jinja2 қате береді
+    
+    # Алдыңғы берген student.html кодына қажетті бос тізімдер
     return render_template('student.html', teacher_tasks=[], tasks=[])
 
 @app.route('/logout')
